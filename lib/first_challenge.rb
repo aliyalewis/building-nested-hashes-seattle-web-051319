61518cf4d20367fa5505d3bf48d5fd75160bde06
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-06 19:23:39 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,15 +1,5 @@
 def first_challenge
-   epic_tragedy = {:montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-     },
-     :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => []
-       }
-     }
+   epic_tragedy = {
+     :montague 
+     :capulet => {}
 end
